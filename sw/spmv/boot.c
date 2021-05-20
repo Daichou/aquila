be7874a13ed6769bfc994d1686dd10d49ab23c11
Daichou <tommy0705c@gmail.com>
2021-05-20 14:23:00 +0800

    mm and spmv

--- sw/spmv/boot.c
+++ sw/spmv/boot.c
@@ -1,15 +1,14 @@
-/*
 // =============================================================================
-//  Program : test.ld
+//  Program : boot.c
 //  Author  : Chun-Jen Tsai
 //  Date    : Jan/14/2020
+// =============================================================================
+//  This is the entry point of the application code. Must be located at
+//  the beginning of the text section in the linker script.
 // -----------------------------------------------------------------------------
 //  Revision information:
 //
 //  None.
-// -----------------------------------------------------------------------------
-//  Description:
-//  This is the linker script of a program compiled to run from DDRx memory.
 // -----------------------------------------------------------------------------
 //  License information:
 //
@@ -52,31 +51,38 @@
 //  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 //  POSSIBILITY OF SUCH DAMAGE.
 // =============================================================================
-*/
-__heap_start = 0x90000000;
-__heap_size  = 0x1FFF0000;
-__stack_top  = 0xBFFF0000;
+#include <stdio.h>
+#include <math.h>
+#define SET_STACK_POINTER 1 // Set sp according to the linker script.
 
-MEMORY
+extern int main(void);
+
+#if SET_STACK_POINTER
+extern unsigned int __stack_top; /* declared in the linker script */
+unsigned int stack_top = (unsigned int) &__stack_top;
+unsigned int sp_store;
+#endif
+
+void boot(void)
 {
-    code_ram   (rx!rw) : ORIGIN = 0x80000000, LENGTH = 0x80000
-    data_ram   (rw!x)  : ORIGIN = 0x80080000, LENGTH = 0x1000000
-}
+#if SET_STACK_POINTER
+    // We must save the return address to the boot loader before
+    // we assign the sp to __stack_top defined in the linker script.
+    asm volatile ("lui t0, %hi(sp_store)");
+    asm volatile ("sw sp, %lo(sp_store)(t0)");
 
-SECTIONS
-{
-    .text :
-    {
-        boot.o(.text)
-        *(.text)
-        /opt/riscv/lib/gcc/riscv32-unknown-elf/10.2.0/rv32im/ilp32/libgcc.a
-    } > code_ram
+    // Set the stack pointer. The application linker script sets
+    // the top address of the stack area to __stack_top.
+    asm volatile("lui t0, %hi(stack_top)");
+    asm volatile("lw  sp, %lo(stack_top)(t0)");
+#endif
 
-    .data :
-    {
-        *(.data)
-        *(.bss)
-        *(.rodata*)
+    main();
 
-    } > data_ram
+#if SET_STACK_POINTER
+    // Now, we must restore the stack pointer of the boot loader
+    // so that we can execute the epilogue of the boot loader properly.
+    asm volatile ("lui t0, %hi(sp_store)");
+    asm volatile ("lw sp, %lo(sp_store)(t0)");
+#endif
 }
